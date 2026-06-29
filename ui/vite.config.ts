8cf3e7ce71ccdc031b0381d07bef0ea5aa230689
polyglot.dev <239993091+poliglots@users.noreply.github.com>
2026-06-29 08:31:54 +0000

    name

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,19 +4,19 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  base: "/brags/",
   publicDir: "../dist",
   build: {
     outDir: "../dist",
     emptyOutDir: false,
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("react-dom")) return "react-dom";
-          if (id.includes("react")) return "react";
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks: (id) => {
+    //       if (id.includes("react-dom")) return "react-dom";
+    //       if (id.includes("react")) return "react";
+    //     },
+    //   },
+    // },
+    // chunkSizeWarningLimit: 1000,
   },
 });
